424d6d3e9081088b77045e04b2d436a9fb02201a
jolu2111 <jolu2111@gmail.com>
2025-01-27 19:43:45 +0100

    Layout

--- Code/TestDeepXDE.py
+++ Code/TestDeepXDE.py
@@ -1,2 +1,23 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
+print('hello')
+# Generate some data
+x = np.linspace(0, 10, 100) 
+y = np.sin(x)
+
+# Create a plot
+plt.plot(x, y)
+plt.plot(x, y, 'o')
+
+# Add title and labels
+plt.title('Simple Sine Wave Plot')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+
+
+# Show the plot
+plt.show()
+plt.plot(x, y, '-o')
+plt.show()
