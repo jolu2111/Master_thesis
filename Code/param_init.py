30d61a26ac72826e2fb15740c9fc96b8a82a3e53
jolu2111 <jolu2111@gmail.com>
2025-05-14 23:06:56 +0200

    played around with multiple layers

--- Code/param_init.py
+++ Code/param_init.py
@@ -4,7 +4,7 @@ def z_score_normalize_simple(x, mean, std):
     """Applies Z-score normalization."""
     return (x - mean) / std
 
-def initialize_param(N, distribute=None, specs=None, normalize=False):
+def initialize_param(N, distribute=None, specs=None, normalize=False, terzaghi=False):
     """
     Initialize training parameters with options to distribute selected parameters.
 
@@ -40,24 +40,35 @@ def initialize_param(N, distribute=None, specs=None, normalize=False):
     >>> params = initialize_param(N=100, distribute=['m', 'mu'], specs=specs, normalize=True)
     
     """
-    ################# Define default specifications for parameters if specs is not provided. #######################
-    default_specs = {
-        't': {'range': 5.0},
-        'm':  {'mean': 1.0,  'std': 0.1, 'lower_multiplier': -5, 'upper_multiplier': 5},
-        'mu': {'mean': 0.6,  'std': 0.05, 'lower_multiplier': -5, 'upper_multiplier': 5},
-        'k':  {'mean': 5.0,  'std': 0.5, 'lower_multiplier': -5, 'upper_multiplier': 5},
-        'y0': {'mean': -0.4, 'std': 0.1, 'lower_multiplier': -5, 'upper_multiplier': 5},
-        'v0': {'mean': 3.0,  'std': 0.5, 'lower_multiplier': -5, 'upper_multiplier': 5},
-    }
+    # ################# Define default specifications for parameters if specs is not provided. #######################
+    # default_specs = {
+    #     't': {'range': 5.0},
+    #     'm':  {'mean': 1.0,  'std': 0.1, 'lower_multiplier': -5, 'upper_multiplier': 5},
+    #     'mu': {'mean': 0.6,  'std': 0.05, 'lower_multiplier': -5, 'upper_multiplier': 5},
+    #     'k':  {'mean': 5.0,  'std': 0.5, 'lower_multiplier': -5, 'upper_multiplier': 5},
+    #     'y0': {'mean': -0.4, 'std': 0.1, 'lower_multiplier': -5, 'upper_multiplier': 5},
+    #     'v0': {'mean': 3.0,  'std': 0.5, 'lower_multiplier': -5, 'upper_multiplier': 5},
+    # }
 
-    if specs is None:
-        specs = default_specs
-    else:
-    # Fill in any missing parameter specs with the defaults.
-        for key, val in default_specs.items():
-            if key not in specs:
-                specs[key] = val
+    # if specs is None:
+    #     specs = default_specs
+    # else:
+    # # Fill in any missing parameter specs with the defaults.
+    #     for key, val in default_specs.items():
+    #         if key not in specs:
+    #             specs[key] = val
     
+    params = {}
+
+    # Prepare a normalization info dictionary if normalization is used.
+    if normalize:
+        norm_info = {}
+        norm_info['t'] = specs['t']
+        # Add normalization info and flag to the dictionary.
+        params['norm_info'] = norm_info
+    else:
+        params['norm_info'] = None
+
     ################## Create time collocation points ############################################################
     if normalize: # If normalization is used, the time collocation points should be normalized as well.
         t_coll = torch.rand(N, 1)
@@ -66,31 +77,17 @@ def initialize_param(N, distribute=None, specs=None, normalize=False):
         t_coll = torch.rand(N, 1)*specs['t']['range']
         t_coll.requires_grad_(True)
 
-    # t_coll = torch.rand(N, 1)*specs['t']['range']
-    # t_coll.requires_grad_(True)
-
-
     # Initialize the output dictionary with the time collocation points.
-    params = {'t_coll': t_coll}
+    params['t_coll'] = t_coll
     
     ############# Initialize parameters based on which to distribution and normalize ##############################
 
     # If distribute is not provided, assume no parameter is distributed.
     if distribute is None:
         distribute = []
-
-    # Also prepare a normalization info dictionary if normalization is used.
-
-    if normalize:
-        norm_info = {}
-        norm_info['t'] = specs['t']
-        # Add normalization info and flag to the dictionary.
-        params['norm_info'] = norm_info
-    else:
-        params['norm_info'] = None
     
     # For each parameter, either sample uniformly (if distributed) or use a constant value.
-    for param in ['m', 'mu', 'k', 'y0', 'v0']:
+    for param in [key for key in specs.keys() if key != 't']:
         spec = specs[param]
         mean = spec['mean']
         std = spec['std']
@@ -99,21 +96,37 @@ def initialize_param(N, distribute=None, specs=None, normalize=False):
         
         if param in distribute:
             # Sample uniformly for distributed parameters.
-            tensor = torch.FloatTensor(N, 1).uniform_(lower, upper) 
-            if normalize:
-                # Apply Z-score normalization
-                tensor = z_score_normalize_simple(tensor, mean, std)
-                # Store normalization info for later inversion in the loss functions.
-                norm_info[param] = {'mean': mean, 'std': std}
-            # tensor.requires_grad_(True)  ################### This is unneecasrry, try without!!
+            tensor = torch.FloatTensor(N, 1).uniform_(lower, upper)
         else:
             # Use a constant value for non-distributed parameters.
             tensor = torch.tensor([[mean]])#, requires_grad=True)
             tensor = tensor.expand(N, -1)
+            
+        if terzaghi and param == 'H':
+            # Terzaghi's model requires a depth parameter.
+            z_coll = tensor* torch.rand(N, 1) # Random depth
+            if normalize:
+                # normalize the depth to between 0 and 1
+                z_coll/= tensor
+            z_coll.requires_grad_(True) 
+            params['z_coll'] = z_coll
+        
+        if normalize:
+            # Apply Z-score normalization
+            tensor = z_score_normalize_simple(tensor, mean, std)
+            # Store normalization info for later inversion in the loss functions.
+            norm_info[param] = {'mean': mean, 'std': std}
+        
         params[param] = tensor
 
     # Define a single time for the boundary condition (same shape as t_coll)
     t0 = torch.zeros_like(t_coll).clone().detach().requires_grad_(True)
+
     params['t0'] = t0
+    if terzaghi:
+        z0 = torch.zeros_like(t_coll).clone().detach().requires_grad_(True)
+        z1 = torch.ones_like(t_coll).clone().detach().requires_grad_(True)
+        params['z0'] = z0
+        params['z1'] = z1
     
     return params
